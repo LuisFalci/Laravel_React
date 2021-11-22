2da94a70bc708dadb221034686706c7f2673afe9
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-11-22 15:20:49 -0300

    Controller e Rota

--- LaravelReact/routes/web.php
+++ LaravelReact/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/', ['as' => 'home', 'uses' => 'PagesController@index']);
+
+Route::apiResource('/', \App\Http\Controllers\PagesController::class);
